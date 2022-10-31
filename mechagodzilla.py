2f914ccc10bf6d641088ad6e35fc50c864863af0
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-10-31 14:13:54 -0400

    robot and dinosaur class creation

---
+++ mechagodzilla.py
@@ -0,0 +1,12 @@
+# Robot/ Mechagodzilla Class and attributes
+
+from weapons import Weapon
+
+class Mechagodzilla:
+    def __init__(self, name):
+        self.name = 'Mechagodzilla'
+        self.health = 100
+        self.active_weapon = Weapon
+
+    def mech_attack(self, Godzilla):
+        pass
